84a6472d1850d56a256def6917ed5990f540c057
Alexandre Santos <alexandre.santozz@gmail.com>
2020-12-21 21:54:27 +0000

    Update djwt to a version that works fine with deno 1.6.1

--- deps.ts
+++ deps.ts
@@ -3,22 +3,22 @@ export {
   assertThrowsAsync,
 } from "https://deno.land/std@0.74.0/testing/asserts.ts";
 
-export { validateJwt } from "https://deno.land/x/djwt@v1.7/validate.ts";
+export { validateJwt } from "https://raw.githubusercontent.com/asantos00/djwt/update-god_crypto-on-v1.7/validate.ts";
 export type {
   JwtObject,
   JwtValidation,
   Validation,
-} from "https://deno.land/x/djwt@v1.7/validate.ts";
+} from "https://raw.githubusercontent.com/asantos00/djwt/update-god_crypto-on-v1.7/validate.ts";
 
 export {
   makeJwt,
   setExpiration,
-} from "https://deno.land/x/djwt@v1.7/create.ts";
+} from "https://raw.githubusercontent.com/asantos00/djwt/update-god_crypto-on-v1.7/create.ts";
 export type {
   Algorithm,
   Jose,
   Payload,
-} from "https://deno.land/x/djwt@v1.7/create.ts";
+} from "https://raw.githubusercontent.com/asantos00/djwt/update-god_crypto-on-v1.7/create.ts";
 
 export { createHttpError } from "https://deno.land/x/oak@v6.3.1/httpError.ts";
 
