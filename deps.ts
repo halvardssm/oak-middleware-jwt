2ab7f1b3daf735361a3a8d6e0259adfbd8b2ae45
Alexandre Santos <alexandre.santozz@gmail.com>
2020-12-21 21:27:42 +0000

    Use export type

--- deps.ts
+++ deps.ts
@@ -2,25 +2,30 @@ export {
   assert,
   assertThrowsAsync,
 } from "https://deno.land/std@0.74.0/testing/asserts.ts";
-export {
+
+export { validateJwt } from "https://deno.land/x/djwt@v1.7/validate.ts";
+export type {
   JwtObject,
   JwtValidation,
-  validateJwt,
   Validation,
 } from "https://deno.land/x/djwt@v1.7/validate.ts";
+
 export {
-  Algorithm,
-  Jose,
   makeJwt,
-  Payload,
   setExpiration,
 } from "https://deno.land/x/djwt@v1.7/create.ts";
+export type {
+  Algorithm,
+  Jose,
+  Payload,
+} from "https://deno.land/x/djwt@v1.7/create.ts";
+
 export { createHttpError } from "https://deno.land/x/oak@v6.3.1/httpError.ts";
-export {
-  Context,
+
+export type {
   HTTPMethods,
   Middleware,
   RouterContext,
   RouterMiddleware,
-  Status,
 } from "https://deno.land/x/oak@v6.3.1/mod.ts";
+export { Context, Status } from "https://deno.land/x/oak@v6.3.1/mod.ts";
